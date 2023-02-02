d58f4ab77179b3b7f54e682dc87b814a9f99c16d
perrynaise <59591850+perrynaise@users.noreply.github.com>
2023-02-02 17:33:57 +1000

    Update deploy-k3s.sh
    Typo in dashboard URL (yaml not yml).

--- deploy-k3s.sh
+++ deploy-k3s.sh
@@ -280,7 +280,7 @@
 
   GITHUB_URL=https://github.com/kubernetes/dashboard/releases
   VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
-  curl https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yml > kubernetes-dashboard/kubernetes-dashboard.yml
+  curl https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml > kubernetes-dashboard/kubernetes-dashboard.yml
   
   echo "done"
 
