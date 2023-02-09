868f1600f2fd215a58e550c35ed72fb998ee0b61
Darren Brown <dbrown@nowitsolutions.com.au>
2023-02-09 17:39:03 +1000

    2 issues to go

--- addons/nautobot/deploy-nautobot.sh
+++ addons/nautobot/deploy-nautobot.sh
@@ -11,7 +11,6 @@
 
 #Create Functions
 
-
 	set_nautovars () {
 
 		# Define nautovar array containing required variables for K3s deployment
@@ -21,13 +20,13 @@
         nautovar_4=("nautoredispw" "$nautoredispw" "This is the paasword that the Nautobot redis will have.")
         nautovar_5=("domain" "$domain" "This is the domain that your services will be available on e.g. 'yourdomain.com'")
 
-	 # Combine awxvar arrays int the awxvars array
-	 awxvars=(
-		 awxvar_1[@]
-		 awxvar_2[@]
-		 awxvar_3[@]
-		 awxvar_4[@]
-		 awxvar_5[@]
+	 # Combine nautovar arrays int the nautovars array
+	 nautovars=(
+		 nautovar_1[@]
+		 nautovar_2[@]
+		 nautovar_3[@]
+		 nautovar_4[@]
+		 nautovar_5[@]
 	 )
 	}
 
@@ -56,12 +55,148 @@
 	Cyan='\033[0;36m'				 # Cyan
 	White='\033[0;37m'				# White
 
-# Get current working directory
+  # Set Nautobot Variables
 
-  k3sdeploypath=$(pwd)
+	set_nautovars
+	nautomissingvars=()
 
+#Missing Variables
 
-  #https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-helm/
-  helm repo add nginx-stable https://helm.nginx.com/stable
+	title="Looking for missing Nautobot Deployment Variables"
+	print_title
+
+	# Loop nautovars looking for missing variables
+
+		COUNT=${#nautovars[@]}
+		for ((i=0; i<$COUNT; i++)); do
+			NAME=${!nautovars[i]:0:1}
+			VALUE=${!nautovars[i]:1:1}
+			DESC=${!nautovars[i]:2:1}
+
+			if [[ -z "${VALUE}" ]]; then
+				echo "Name: ${NAME}"
+				printf "Value: ${Red}${NAME} is undefined\n${Color_Off}"
+			echo "Description: ${DESC}"
+			printf ${White}"=%.0s"	$(seq 1 100)${Color_Off}
+			printf "\n${Color_Off}"
+			nautomissingvars+=( "nautovar_$(expr $i + 1)[@]" )
+			fi
+		done
+
+	# Loop nautomissingvars to give user option to define any missing variables
+
+		COUNT=${#nautomissingvars[@]}
+		for ((i=0; i<$COUNT; i++)); do
+			NAME=${!nautomissingvars[i]:0:1}
+			VALUE=${!nautomissingvars[i]:1:1}
+			DESC=${!nautomissingvars[i]:2:1}
+
+			read -p "Would you like to provide a value for $NAME? " -r
+			echo		# (optional) move to a new line
+			if [[ $REPLY =~ ^[Yy]$ ]]; then
+				read -p "Enter value for $NAME: " $NAME
+			fi
+		done
+
+	printf "${Green}Done\n${Color_Off}"
+
+# Update nauto Variables
+
+	set_nautovars
+	clear
+
+# Loop nautovars to display variables to be used for K3s deployment.
+
+	title="Variables to be using in K3s Deployment"
+	print_title
+
+	COUNT=${#nautovars[@]}
+	for ((i=0; i<$COUNT; i++)); do
+		NAME=${!nautovars[i]:0:1}
+		VALUE=${!nautovars[i]:1:1}
+		DESC=${!nautovars[i]:2:1}
+
+		if [[ -z "${VALUE}" ]]; then
+			echo "Name: ${NAME}"
+			printf "Value: ${Red}${NAME} is undefined\n${Color_Off}"
+			echo "Description: ${DESC}"
+			printf ${White}"=%.0s"	$(seq 1 100)${Color_Off}
+			printf "\n${Color_Off}"
+		else
+			printf "Name: ${Cyan}${NAME}\n${Color_Off}"
+			printf "Value: ${Green}${VALUE}\n${Color_Off}"
+			printf "Description: ${White}${DESC}\n${Color_Off}"
+			printf ${Blue}"=%.0s"	$(seq 1 100) \n
+			printf "\n${Color_Off}"
+		fi
+	done
+
+	printf "${Green}Done\n${Color_Off}"
+
+#Create 'nautobot.yml' file
+
+	title="Creating 'nautobot.yml' file"
+	print_title
+
+	sed -i "s/nautosubd/$nautosubd/g" nautobot.yml
+	sed -i "s/domain/$domain/g" nautobot.yml
+    sed -i "s/nautosqlpw/$nautosqlpw/g" nautobot.yml
+	sed -i "s/nautoredispq/$nautoredispw/g" nautobot.yml
+
+	printf "${Green}Done\n${Color_Off}"
+
+#Deploy nautobot with helm
+
+  #https://github.com/nautobot/helm-charts
+  helm repo add nautobot https://nautobot.github.io/helm-charts/
   helm repo update
-  helm install "$ingname" nginx-stable/nginx-ingress --namespace "$ingns" --create-namespace
+  helm install "$ingname" nautobot/nautobot -f ./nautobot.yml --set-file nautobot.config=./nautobot_config.py --namespace "$ingns" --create-namespace
+
+#Wait for Nautobot to be Ready
+
+	title="Waiting for Nautobot to be Ready"
+	print_title
+
+	nautopods=$(kubectl get pods -n ${nautons} -o 'jsonpath={..metadata.name}')
+	IFS='/ ' read -r -a nautopods <<< "$nautopods"
+	
+	#wait for there to be 6 pods in the nautobot namespace
+	while [ ${#nautopods[@]} -ne 6 ]
+	do
+		nautopods=$(kubectl get pods -n ${nautons} -o 'jsonpath={..metadata.name}')
+		IFS='/ ' read -r -a nautopods <<< "$nautopods"
+	done
+	
+	#wait for those 6 pods to be in a ready state
+	for i in "${nautopods[@]}"; do
+		kubectl wait --for=condition=Ready pod/${i}
+	done
+
+	printf "${Green}Done\n${Color_Off}"
+
+
+#Wait for Certificate to be assigned
+
+	title="Waiting for Nautobot Certificate to be Ready"
+	print_title
+
+	awxcert=$(kubectl get certificate -n ${nautons} -o 'jsonpath={..metadata.name}')
+	IFS='/ ' read -r -a nautocert <<< "$nautocert"
+	for i in "${nautocert[@]}"; do
+		kubectl wait --for=condition=Ready certificate/${i}
+	done
+
+	printf "${Green}Done\n${Color_Off}"
+
+#Deployment of Nautobot complete
+
+	title="Nautobot Deployment Complete"
+	print_title
+
+!TODO #1	#pass=$(kubectl get secret nautobot-admin-password -o jsonpath="{.data.password}" | base64 --decode)
+	
+	printf "${Green}You can now access your Nautobot Dashboard at https://${nautosubd}.${domain}\n${Color_Off}"
+	printf "${Green}Username: admin\n${Color_Off}"
+!TODO #2	#printf "${Green}Password: ${pass}\n${Color_Off}"
+	
+	pass=
--- addons/nautobot/nautobot_config.py
+++ addons/nautobot/nautobot_config.py
@@ -12,73 +12,75 @@ from nautobot.core.settings_funcs import is_truthy
 #                       #
 #########################
 
-AUTHENTICATION_BACKENDS = [
-    'django_auth_ldap.backend.LDAPBackend',
-    'nautobot.core.authentication.ObjectPermissionBackend',
-]
+###### Example LDAP config ######
 
-import ldap
-
-from django_auth_ldap.config import LDAPSearch, LDAPGroupQuery, NestedActiveDirectoryGroupType
-
-REMOTE_AUTH_ENABLED = True
-
-# Server URI
-AUTH_LDAP_SERVER_URI = os.getenv("AUTH_LDAP_SERVER_URI", "")
-AUTH_LDAP_BIND_DN = os.getenv("AUTH_LDAP_BIND_DN", "")
-AUTH_LDAP_BIND_PASSWORD = os.getenv("AUTH_LDAP_BIND_PASSWORD", "")
-
-# The following may be needed if you are binding to Active Directory.
-AUTH_LDAP_CONNECTION_OPTIONS = {
-    ldap.OPT_REFERRALS: 0
-}
-
-# Set the DN and password for the Nautobot service account.
-AUTH_LDAP_GROUP_TYPE = NestedActiveDirectoryGroupType()
-
-# Include this `ldap.set_option` call if you want to ignore certificate errors. This might be needed to accept a self-signed cert.
-ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-
-
-# If a user's DN is producible from their username, we don't need to search.
-# - not wanted for those who keep their users in a variety of subfolders e.g. engineering.users
-#AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,ou=users,dc=example,dc=com"
-
-# This search matches users with the sAMAccountName equal to the provided username. This is required if the user's
-# username is not in their DN (Active Directory).
-AUTH_LDAP_USER_SEARCH = LDAPSearch('DC=autothis,DC=com,DC=au',
-                                   ldap.SCOPE_SUBTREE,
-                                   "(sAMAccountName=%(user)s)")
-
-# users permitted to log in must be in one of these AD groups
-AUTH_LDAP_REQUIRE_GROUP = (
-    LDAPGroupQuery("CN=Nautobot - Engineer,OU=Nautobot,OU=Security Groups,DC=autothis,DC=com,DC=au")
-)
-
-# setting user flags by group
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    # as per main config, we might need "is_active" duplicating AUTH_LDAP_REQUIRE_GROUP.
-    # try recycling the same union object instead of rebuilding from strings.
-    "is_active": AUTH_LDAP_REQUIRE_GROUP,
-    "is_staff": "CN=Nautobot - Engineer,OU=Nautobot,OU=Security Groups,DC=autothis,DC=com,DC=au",
-    # "is_superuser": ""  # we choose never to make more superusers by ldap
-}
-
-# Mirroring LDAP groups with filter to allowed list
-# as per django doc, this is not favored, instead use find_groups
-# more: https://stackoverflow.com/questions/52062136/django-auth-ldap-mirror-groups-not-working
-# https://django-auth-ldap.readthedocs.io/en/latest/permissions.html
-AUTH_LDAP_MIRROR_GROUPS = ["Nautobot - Engineer"]
-
-# AUTH_LDAP_FIND_GROUP_PERMS = True
-
+#AUTHENTICATION_BACKENDS = [
+#    'django_auth_ldap.backend.LDAPBackend',
+#    'nautobot.core.authentication.ObjectPermissionBackend',
+#]
 #
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch('OU=Nautobot,OU=Security Groups,DC=autothis,DC=com,DC=au',
-                                    ldap.SCOPE_SUBTREE,
-                                    '(objectClass=group)')
-
-LOG_LDAP_TO_CONSOLE = True  # preempts file and should not be left running in prod
-# LOG_LDAP_TO_FILE = True  # busted and commented out
+#import ldap
+#
+#from django_auth_ldap.config import LDAPSearch, LDAPGroupQuery, NestedActiveDirectoryGroupType
+#
+#REMOTE_AUTH_ENABLED = True
+#
+## Server URI
+#AUTH_LDAP_SERVER_URI = os.getenv("AUTH_LDAP_SERVER_URI", "")
+#AUTH_LDAP_BIND_DN = os.getenv("AUTH_LDAP_BIND_DN", "")
+#AUTH_LDAP_BIND_PASSWORD = os.getenv("AUTH_LDAP_BIND_PASSWORD", "")
+#
+## The following may be needed if you are binding to Active Directory.
+#AUTH_LDAP_CONNECTION_OPTIONS = {
+#    ldap.OPT_REFERRALS: 0
+#}
+#
+## Set the DN and password for the Nautobot service account.
+#AUTH_LDAP_GROUP_TYPE = NestedActiveDirectoryGroupType()
+#
+## Include this `ldap.set_option` call if you want to ignore certificate errors. This might be needed to accept a self-signed cert.
+#ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+#
+#
+## If a user's DN is producible from their username, we don't need to search.
+## - not wanted for those who keep their users in a variety of subfolders e.g. engineering.users
+##AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,ou=users,dc=example,dc=com"
+#
+## This search matches users with the sAMAccountName equal to the provided username. This is required if the user's
+## username is not in their DN (Active Directory).
+#AUTH_LDAP_USER_SEARCH = LDAPSearch('DC=autothis,DC=com,DC=au',
+#                                   ldap.SCOPE_SUBTREE,
+#                                   "(sAMAccountName=%(user)s)")
+#
+## users permitted to log in must be in one of these AD groups
+#AUTH_LDAP_REQUIRE_GROUP = (
+#    LDAPGroupQuery("CN=Nautobot - Engineer,OU=Nautobot,OU=Security Groups,DC=autothis,DC=com,DC=au")
+#)
+#
+## setting user flags by group
+#AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+#    # as per main config, we might need "is_active" duplicating AUTH_LDAP_REQUIRE_GROUP.
+#    # try recycling the same union object instead of rebuilding from strings.
+#    "is_active": AUTH_LDAP_REQUIRE_GROUP,
+#    "is_staff": "CN=Nautobot - Engineer,OU=Nautobot,OU=Security Groups,DC=autothis,DC=com,DC=au",
+#    # "is_superuser": ""  # we choose never to make more superusers by ldap
+#}
+#
+## Mirroring LDAP groups with filter to allowed list
+## as per django doc, this is not favored, instead use find_groups
+## more: https://stackoverflow.com/questions/52062136/django-auth-ldap-mirror-groups-not-working
+## https://django-auth-ldap.readthedocs.io/en/latest/permissions.html
+#AUTH_LDAP_MIRROR_GROUPS = ["Nautobot - Engineer"]
+#
+## AUTH_LDAP_FIND_GROUP_PERMS = True
+#
+##
+#AUTH_LDAP_GROUP_SEARCH = LDAPSearch('OU=Nautobot,OU=Security Groups,DC=autothis,DC=com,DC=au',
+#                                    ldap.SCOPE_SUBTREE,
+#                                    '(objectClass=group)')
+#
+#LOG_LDAP_TO_CONSOLE = True  # preempts file and should not be left running in prod
+## LOG_LDAP_TO_FILE = True  # busted and commented out
 
 
 #########################
@@ -103,9 +105,9 @@ LOG_LDAP_TO_CONSOLE = True  # preempts file and should not be left running in pr
 #                       #
 #########################
 
-# Enable installed plugins. Add the name of each plugin to the list.
-#PLUGINS = ["nautobot_device_onboarding","nautobot-golden-config","nautobot_plugin_nornir"]
-PLUGINS = ["nautobot_ssot","nautobot_ssot_vsphere","nautobot_golden_config","nautobot_plugin_nornir"]
+# Enable installed plugins. Add the name of each plugin to the list. This requires a custom image so extra plugins have been disabled.
+#PLUGINS = ["nautobot_ssot_vsphere","nautobot_device_onboarding","nautobot-golden-config","nautobot_plugin_nornir"]
+PLUGINS = ["nautobot_plugin_nornir"]
 
 # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
@@ -124,24 +126,24 @@ PLUGINS_CONFIG = {
             },
         },
     },
-    "nautobot_golden_config": {
-        "per_feature_bar_width": 0.15,
-        "per_feature_width": 13,
-        "per_feature_height": 4,
-        "enable_backup": True,
-        "enable_compliance": True,
-        "enable_intended": True,
-        "enable_sotagg": True,
-        "sot_agg_transposer": None,
-        "platform_slug_map": None,
-    },
-    "nautobot_ssot": {
-        "hide_example_jobs": False,  # defaults to False if unspecified
-    },
-    "nautobot_ssot_vsphere": {
-        "VSPHERE_URI": os.getenv("VSPHERE_URI"),
-        "VSPHERE_USERNAME": os.getenv("VSPHERE_USERNAME"),
-        "VSPHERE_PASSWORD": os.getenv("VSPHERE_PASSWORD"),
-        "VSPHERE_VERIFY_SSL": is_truthy(os.getenv("VSPHERE_VERIFY_SSL", False)),
-    }
+    #"nautobot_golden_config": {
+    #    "per_feature_bar_width": 0.15,
+    #    "per_feature_width": 13,
+    #    "per_feature_height": 4,
+    #    "enable_backup": True,
+    #    "enable_compliance": True,
+    #    "enable_intended": True,
+    #    "enable_sotagg": True,
+    #    "sot_agg_transposer": None,
+    #    "platform_slug_map": None,
+    #},
+    #"nautobot_ssot": {
+    #    "hide_example_jobs": False,  # defaults to False if unspecified
+    #},
+    #"nautobot_ssot_vsphere": {
+    #    "VSPHERE_URI": os.getenv("VSPHERE_URI"),
+    #    "VSPHERE_USERNAME": os.getenv("VSPHERE_USERNAME"),
+    #    "VSPHERE_PASSWORD": os.getenv("VSPHERE_PASSWORD"),
+    #    "VSPHERE_VERIFY_SSL": is_truthy(os.getenv("VSPHERE_VERIFY_SSL", False)),
+    #}
 }
